9bfaaa8690297fbd6e014603f700850384fb44ed
melange <34423707+melange@users.noreply.github.com>
2023-08-14 01:44:12 +0300

    implemented clusters

--- src/components/map/MapWithSidePanel.tsx
+++ src/components/map/MapWithSidePanel.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from 'react';
 import GoogleMap from 'google-maps-react-markers';
+import useSupercluster from '../../hooks/useSupercluster';
 import Marker from './Marker';
 import SidePanel from '../sidePanel/SidePanel';
 import PropertyDetails from '../propertyDetails/PropertyDetails';
@@ -12,14 +13,44 @@ type Props = {
 function MapWithSidePanel(props: Props) {
   const { locations } = props;
   const mapRef = useRef(null);
+  const [bounds, setBounds] = useState([69.15693006286622, 41.231565100006854, 69.3232699371338, 41.367364210349656]);
+  const [zoom, setZoom] = useState(13);
   const [mapReady, setMapReady] = useState(false);
   const [highlighted, setHighlighted] = useState<string | undefined>();
   const [sidePanelOpen, setSidePanelOpen] = useState(false);
   const [filteredLocations, setFilteredLocations] = useState(locations);
 
+  const points = filteredLocations.map((location) => ({
+    type: 'Feature',
+    properties: { cluster: false, locationId: location.id, price: location.price },
+    geometry: {
+      type: 'Point',
+      coordinates: [
+        parseFloat(location.location.lng.toString()),
+        parseFloat(location.location.lat.toString()),
+      ],
+    },
+  }));
+
+  const { clusters, supercluster } = useSupercluster({
+    points,
+    bounds,
+    zoom,
+    options: { radius: 40, maxZoom: 20 },
+  });
+
   const onMarkerClick = (markerId: string) => {
     setHighlighted(markerId);
     setSidePanelOpen(true);
+  };
+
+  const onClusterClick = (cluster, longitude, latitude) => {
+    const expansionZoom = Math.min(
+      supercluster.getClusterExpansionZoom(cluster.id),
+      20,
+    );
+    mapRef.current.setZoom(expansionZoom);
+    mapRef.current.panTo({ lat: latitude, lng: longitude });
   };
 
   const onSidePanelClose = () => {
@@ -54,8 +85,51 @@ function MapWithSidePanel(props: Props) {
         defaultCenter={{ lat: 41.2995, lng: 69.2401 }}
         defaultZoom={13}
         onGoogleApiLoaded={onGoogleApiLoaded}
+        yesIWantToUseGoogleMapApiInternals
+        onChange={({ zoom, bounds }) => {
+          setZoom(zoom);
+          const ne = bounds.getNorthEast();
+          const sw = bounds.getSouthWest();
+          setBounds([sw.lng(), sw.lat(), ne.lng(), ne.lat()]);
+        }}
       >
-        {filteredLocations.map((location) => (
+        {console.log(clusters)}
+        {clusters.map((cluster) => {
+          const [longitude, latitude] = cluster.geometry.coordinates;
+          const {
+            cluster: isCluster,
+            point_count: pointCount,
+          } = cluster.properties;
+
+          if (isCluster) {
+            return (
+              <Marker
+                key={cluster.id}
+                text={`${cluster.properties.point_count} properties`}
+                // text={`${cluster.price}`}
+                lat={latitude}
+                lng={longitude}
+                markerId={cluster.id}
+                highlighted={highlighted === cluster.id}
+                onClick={() => onClusterClick(cluster, longitude, latitude)}
+              />
+            );
+          }
+
+          return (
+            <Marker
+              key={cluster.id}
+              text={`$${cluster.properties.price}`}
+              lat={latitude}
+              lng={longitude}
+              markerId={cluster.properties.locationId}
+              highlighted={highlighted === cluster.properties.locationId}
+              onClick={onMarkerClick}
+            />
+          );
+        })}
+
+        {/* {filteredLocations.map((location) => (
           <Marker
             key={location.id}
             text={`${location.price}`}
@@ -65,7 +139,7 @@ function MapWithSidePanel(props: Props) {
             highlighted={highlighted === location.id}
             onClick={onMarkerClick}
           />
-        ))}
+        ))} */}
 
       </GoogleMap>
     </div>
---
+++ src/hooks/useSupercluster.ts
@@ -0,0 +1,43 @@
+import React, { useState, useRef } from 'react';
+import { useDeepCompareEffectNoCheck } from 'use-deep-compare-effect';
+import { dequal } from 'dequal';
+import Supercluster from 'supercluster';
+
+function useSupercluster(_ref) {
+  const {
+    points, bounds, zoom, options,
+  } = _ref;
+
+  // console.log(points);
+  // console.log(zoom);
+
+  const superclusterRef = useRef();
+  const pointsRef = useRef();
+
+  const [clusters, setClusters] = useState([]);
+  const zoomInt = Math.round(zoom);
+
+  useDeepCompareEffectNoCheck(() => {
+    if (!superclusterRef.current || dequal(pointsRef.current, points) || !dequal(superclusterRef.current.options, options)) {
+      superclusterRef.current = new Supercluster(options);
+      superclusterRef.current.load(points);
+    }
+
+    if (bounds) {
+      setClusters(superclusterRef.current.getClusters(bounds, zoomInt));
+      // console.log(bounds);
+      // console.log(superclusterRef.current);
+      // console.log(zoomInt);
+      // console.log(superclusterRef.current.getClusters(bounds, zoomInt));
+    }
+
+    pointsRef.current = points;
+  }, [points, bounds, zoomInt, options]);
+
+  return {
+    clusters,
+    supercluster: superclusterRef.current,
+  };
+}
+
+export default useSupercluster;
