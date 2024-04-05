7a8a6b2ef13ff18a05639ca0ba044a4d3523e2da
melange <34423707+melange@users.noreply.github.com>
2024-04-05 23:34:02 +0400

    scroll bug attempt 2

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import {
+  Html, Head, Main, NextScript,
+} from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="overflow-x-hidden">
       <Head />
-      <body>
+      <body className="relative overflow-x-hidden">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
