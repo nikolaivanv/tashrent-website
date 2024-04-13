713408687919eafd90aeb44856f89452efd4826a
melange <34423707+melange@users.noreply.github.com>
2024-04-14 00:50:55 +0400

    get files from cloudfront

--- src/helpers/api-utils.tsx
+++ src/helpers/api-utils.tsx
@@ -21,41 +21,50 @@ import AWS from 'aws-sdk';
 // const s3 = new AWS.S3();
 
 async function readListingsFromS3(): Promise<IPropertyListing[]> {
-  let listings: IPropertyListing[] = [];
+  //let listings: IPropertyListing[] = [];
   // const params = {
   //   Bucket: process.env.AWS_S3_BUCKET,
   //   Key: process.env.AWS_S3_KEY,
   // };
 
   try {
-    console.log('Downloading data from S3');
+    console.log('Downloading all listings from S3');
     // const data = await s3.getObject(params).promise();
-    listings = fetch('https://tashrent.s3.us-east-2.amazonaws.com/listings.json', {
-      headers: new Headers({
-        Authorization: 'AWS ' + `${process.env.AWS_ACCESS_KEY_ID}:${process.env.AWS_SECRET_ACCESS_KEY}`,
-      }),
-    }).then((response) => console.log(response)); // .then((response) => response.json())
+    const listings = await fetch('https://dfkg1j9k2eshd.cloudfront.net/listings.json').then((response) => response.json());
+    //console.log('data: ', data);
     // const data = fetch('https://tashrent.s3.us-east-2.amazonaws.com/listings.json').then((response) => response.json());
-    // listings = JSON.parse(data.toString());
+    //listings = JSON.parse(data.toString());
     return listings;
   } catch (err) {
     return null;
   }
 }
 
-export async function getAllProperties(): Promise<IPropertyListing[]> {
-  // const listings = await readListingsFromFile();
-  console.log('getAllProperties');
-  const listings = await readListingsFromS3();
-  return listings;
+async function readListingFromS3(propertyId: string): Promise<IPropertyListing> {
+  try {
+    console.log('Downloading a single listing from S3');
+    // const data = await s3.getObject(params).promise();
+    const listing = await fetch(`https://dfkg1j9k2eshd.cloudfront.net/${propertyId}.json`).then((response) => response.json());
+    // const data = fetch('https://tashrent.s3.us-east-2.amazonaws.com/listings.json').then((response) => response.json());
+    return listing;
+  } catch (err) {
+    return null;
+  }
 }
+
+// export async function getAllProperties(): Promise<IPropertyListing[]> {
+//   // const listings = await readListingsFromFile();
+//   console.log('getAllProperties');
+//   const listings = await readListingsFromS3();
+//   return listings;
+// }
 
 export async function getPropertyById(propertyId: string): Promise<IPropertyListing | undefined> {
   // const listings = await readListingsFromFile();
   console.log('getPropertyById');
-  const listings = await readListingsFromS3();
-  const property = listings.find((p) => p.id === propertyId);
-  return property;
+  const listing = await readListingFromS3(propertyId);
+  //const property = listings.find((p) => p.id === propertyId);
+  return listing;
 }
 
 export async function getAllPropertiesForMap(): Promise<IPropertyForMap[]> {
